d634d20441a65638da7c77a7aa23398b76251a0c
dogolaa_ <lucasdogo01@gmail.com>
2025-08-18 11:19:18 -0300

    Atualiza o código do ping-pong para suportar múltiplos processos e melhora a configuração do ambiente de desenvolvimento

--- Aula2/ping-pong-cyclic.c
+++ Aula2/ping-pong-cyclic.c
@@ -2,38 +2,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char **argv)
-{
-	const int PING_PONG_LIMIT = 1000000;
+int main(int argc, char **argv) {
+    const int PING_PONG_LIMIT = 10;
 
-	// Initialize the MPI environment
-	MPI_Init(NULL, NULL);
-	// Find out rank, size
-	int world_rank;
-	MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-	int world_size;
-	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-	// MPI_Request request;
+    // Inicializa o ambiente MPI
+    MPI_Init(NULL, NULL);
+    // Descobre o rank e o tamanho do mundo
+    int world_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    int world_size;
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
-	// We are assuming 2 processes for this task
-	if (world_size != 20)
-	{
-		fprintf(stderr, "World size must be two for %s\n", argv[0]);
-		MPI_Abort(MPI_COMM_WORLD, 1);
-	}
+    // Garante que haja pelo menos 2 processos
+    if (world_size < 2) {
+        fprintf(stderr, "World size must be greater than one for %s\n", argv[0]);
+        MPI_Abort(MPI_COMM_WORLD, 1);
+    }
 
-	int ping_pong_count = 0;
-	int partner_rank = (world_rank + 1) % 20;
-	while (ping_pong_count < PING_PONG_LIMIT)
-	{
+    int ping_pong_count = 0;
+    
+    // Calcula o rank do próximo processo no anel
+    int next_rank = (world_rank + 1) % world_size;
+    // Calcula o rank do processo anterior no anel
+    // Adicionamos world_size antes do módulo para lidar com o caso do rank 0
+    int prev_rank = (world_rank - 1 + world_size) % world_size;
 
-		// Increment the ping pong count before you send it
-		ping_pong_count++;
-		MPI_Send(&ping_pong_count, 1, MPI_INT, (partner_rank + 1) % world_size, 0, MPI_COMM_WORLD);
-		printf("%d sent and incremented ping_pong_count %d to %d\n", world_rank, ping_pong_count, partner_rank);
-		MPI_Recv(&ping_pong_count, 1, MPI_INT, (partner_rank - 1 + world_size) % world_size, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-		printf("%d received ping_pong_count %d from %d\n", world_rank, ping_pong_count, partner_rank);
+    // Apenas o processo com rank 0 inicia a bola
+    if (world_rank == 0) {
+        printf("Processo %d está iniciando o ping-pong com %d processos.\n", world_rank, world_size);
+        ping_pong_count++;
+        MPI_Send(&ping_pong_count, 1, MPI_INT, next_rank, 0, MPI_COMM_WORLD);
+        printf("Processo %d enviou e incrementou ping_pong_count %d para o processo %d.\n", world_rank, ping_pong_count, next_rank);
+    }
 
-		MPI_Finalize();
-	}
+    // Loop principal para receber e enviar a mensagem
+    while (ping_pong_count < PING_PONG_LIMIT) {
+        // Recebe a mensagem do processo anterior
+        MPI_Recv(&ping_pong_count, 1, MPI_INT, prev_rank, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        printf("Processo %d recebeu ping_pong_count %d do processo %d.\n", world_rank, ping_pong_count, prev_rank);
+
+        // Se o limite foi atingido, não envia mais e encerra
+        if (ping_pong_count >= PING_PONG_LIMIT) {
+            break;
+        }
+
+        // Incrementa o contador e envia para o próximo processo
+        ping_pong_count++;
+        MPI_Send(&ping_pong_count, 1, MPI_INT, next_rank, 0, MPI_COMM_WORLD);
+        printf("Processo %d enviou e incrementou ping_pong_count %d para o processo %d.\n", world_rank, ping_pong_count, next_rank);
+    }
+
+    // Finaliza o ambiente MPI
+    MPI_Finalize();
 }
